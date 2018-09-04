e044c64a0fc4557602ea3d59323b62cb7caf46cd
Miguel Castro <miacastroco@unal.edu.co>
2018-09-04 00:49:21 +0000

    First Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,31 @@
+class BooksController < ApplicationController
+    def index
+        books = Book.all
+       respond_to do |format|
+           format.html {render html: "Hello World", status:200}
+           format.json {render json: books, status:200}
+       end
+    end
+    def show
+        book =Book.find(params[:id])
+        respond_to do |format|
+           format.json {render json: book, status:200}
+       end
+    end
+    def create
+        book = Book.new(params)
+        if book.save
+            respond_to do |format|
+                format.json {render json: book, status:201}
+            end
+        else
+            respond_to do |format|
+                format.json {render json: books.errors, status: :unprocessable_entity}
+            end
+        end
+    end
+    
+    def params_book
+       params.permit(:name, :year, :code, :available) 
+    end
+end
